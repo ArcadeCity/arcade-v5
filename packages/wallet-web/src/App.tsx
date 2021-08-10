b534f0f1e5451b4953058157a573432ddedf5d1f
Christopher David <chris@arcade.city>
2021-08-10 14:38:33 -0400

    Start aliasing Button - tsconfig tweaks

--- packages/wallet-web/src/App.tsx
+++ packages/wallet-web/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { Button } from '@arcadecity/components'
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
+        <Button />
         <p>Wallet Test</p>
       </header>
     </div>
