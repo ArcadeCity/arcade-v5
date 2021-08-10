c44b38634e245e4a88b4991717fa7f5a14df54ce
Christopher David <chris@arcade.city>
2021-08-10 17:56:29 -0400

    Swap in WalletApp component

---
+++ packages/components/src/components/WalletApp/WalletApp.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react'
+import { Alert, View } from 'react-native'
+import { palette } from '../../theme'
+
+export const WalletApp = () => {
+  return (
+    <React.StrictMode>
+      <View style={{ flex: 1, backgroundColor: palette.purple }}></View>
+    </React.StrictMode>
+  )
+}
--- packages/wallet-web/src/index.tsx
+++ packages/wallet-web/src/index.tsx
@@ -11,7 +11,7 @@ import { AppRegistry } from 'react-native-web'
 
 import './index.css'
 
-import { CityApp } from '@arcadecity/components/src/components/CityApp'
+import { WalletApp } from '@arcadecity/components/src/components/WalletApp'
 
 const render = (AppComponent: ComponentType) => {
   AppRegistry.registerComponent('city', () => AppComponent)
@@ -20,4 +20,4 @@ const render = (AppComponent: ComponentType) => {
   })
 }
 
-render(CityApp)
+render(WalletApp)
