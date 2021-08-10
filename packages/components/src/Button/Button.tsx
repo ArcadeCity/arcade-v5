d44563fd12f490b7a9c4b120f741d948e42c0e81
Christopher David <chris@arcade.city>
2021-08-10 15:08:51 -0400

    Call test core func but ugly relative path

--- packages/components/src/Button/Button.tsx
+++ packages/components/src/Button/Button.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
+import { hello } from '../../../core/src'
 
 export const Button = () => {
-  return <button>Test Button</button>
+  return <button onClick={hello}>Test Button</button>
 }
