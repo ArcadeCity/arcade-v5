cf8582d2675d9529caa631e6d6717905196bfb6e
Christopher David <chris@arcade.city>
2021-08-10 17:52:26 -0400

    Swap out wallet-web w city-web starter

--- packages/wallet-web/src/index.tsx
+++ packages/wallet-web/src/index.tsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+// Security precaution
+;(window as any).eval = global.eval = (payload: string) => {
+  const error = new Error(`This app does not allow window.eval().`)
+  Object.assign(error, { payload })
+
+  throw error
+}
+
+import { ComponentType } from 'react'
+import { AppRegistry } from 'react-native-web'
+
 import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+import { CityApp } from '@arcadecity/components/src/components/CityApp'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+const render = (AppComponent: ComponentType) => {
+  AppRegistry.registerComponent('city', () => AppComponent)
+  AppRegistry.runApplication('city', {
+    rootTag: document.getElementById('root'),
+  })
+}
+
+render(CityApp)
