a267ebf1df56d1d9f2e471edd75f357001051654
Christopher David <chris@arcade.city>
2021-08-10 18:11:11 -0400

    update env var for mobile magic

--- packages/core/src/magic/magic.ts
+++ packages/core/src/magic/magic.ts
@@ -1,6 +1,6 @@
 import { Magic as MagicSDK } from '@magic-sdk/react-native'
 
-export const magic = new MagicSDK(process.env.MAGIC_SDK_PK)
+export const magic = new MagicSDK(process.env.REACT_APP_MAGIC_SDK_PK)
 
 export class Magic {
   sdk: MagicSDK
