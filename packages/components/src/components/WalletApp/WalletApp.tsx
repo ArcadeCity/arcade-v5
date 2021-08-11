e296f42caf0d33c7cdb62303f03d0caba2503370
Christopher David <chris@arcade.city>
2021-08-10 21:13:58 -0400

    [wallet-web] Authenticate with Ceramic (#4)
    * Pull in Ceramic service
    
    * Ceramic service install pkgs, tsc 0
    
    * Authenticate with Ceramic using hardcoded seed
    
    * Swap out 3id resolver w key-did for deterministic auth

--- packages/components/src/components/WalletApp/WalletApp.tsx
+++ packages/components/src/components/WalletApp/WalletApp.tsx
@@ -1,12 +1,72 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import { View } from 'react-native'
-import { magic } from '@arcadecity/core'
+import { Ceramic, magic, provider } from '@arcadecity/core'
 import { palette } from '../../theme'
+import { ethers } from 'ethers'
+
+async function sha256(message: any) {
+  // encode as UTF-8
+  const msgBuffer = new TextEncoder().encode(message)
+
+  // hash the message
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer)
+
+  // convert ArrayBuffer to Array
+  const hashArray = Array.from(new Uint8Array(hashBuffer))
+
+  // convert bytes to hex string
+  const hashHex = hashArray
+    .map((b) => ('00' + b.toString(16)).slice(-2))
+    .join('')
+  return hashHex
+}
 
 export const WalletApp = () => {
   const [email, setEmail] = useState('')
   const [userMetadata, setUserMetadata] = useState<any>()
   const [isLoggingIn, setIsLoggingIn] = useState(false)
+
+  useMemo(async () => {
+    if (userMetadata) {
+      console.log('Now lets auth ceramic')
+      const ceramic = new Ceramic()
+
+      const signer = provider.getSigner()
+      // const address = await signer.getAddress()
+      const originalMessage = ''
+      const signedMessage = await signer.signMessage(originalMessage)
+
+      // const messageHash = ethers.utils.id('Hello World')
+      // console.log('messageHash:', messageHash)
+      const dis = ethers.utils.arrayify(signedMessage)
+      console.log('DIS', dis)
+
+      // let flatSig = await signer.signMessage(dis)
+      // console.log(flatSig)
+
+      // const wat = Buffer.from(flatSig)
+      // console.log(wat)
+
+      ceramic.setup()
+      ceramic.authenticate(dis.slice(0, 32))
+
+      // console.log('signedMessage:', signedMessage)
+
+      // const encoder = new TextEncoder()
+      // const sha = await sha256(signedMessage)
+      // console.log('sha:', sha)
+      // const seed = encoder.encode(sha)
+      // console.log(seed)
+      // console.log(seed.length)
+
+      // const wat = Buffer.from(signedMessage)
+      // console.log(wat)
+      // console.log(wat.length)
+
+      // console.log('signedMessage:', signedMessage)
+      // console.log(ceramic)
+    }
+  }, [userMetadata])
 
   useEffect(() => {
     magic.user.isLoggedIn().then((magicIsLoggedIn) => {
