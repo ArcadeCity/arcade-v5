20d5d34e7b5a908f3adafdd7cf88dfd72a0dd4de
Christopher David <chris@arcade.city>
2021-08-12 17:19:39 -0400

    Pull in Storybook folder

---
+++ packages/components/storybook/toggle-storybook.tsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react'
+
+const USE_STORYBOOK = false
+
+export function ToggleStorybook(props) {
+  const [showStorybook, setShowStorybook] = useState(false)
+  const [StorybookUIRoot, setStorybookUIRoot] = useState(null)
+
+  useEffect(() => {
+    if (USE_STORYBOOK) {
+      setStorybookUIRoot(() => require('./storybook').StorybookUIRoot)
+      setShowStorybook(true)
+    }
+  }, [])
+
+  if (showStorybook) {
+    return StorybookUIRoot ? <StorybookUIRoot /> : null
+  } else {
+    return props.children
+  }
+}
