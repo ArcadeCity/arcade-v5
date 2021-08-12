ef648270ff31b31604e3368aa1c745fe0d116a15
Christopher David <chris@arcade.city>
2021-08-12 12:13:57 -0400

    Axe StrictMode for now

--- packages/components/src/components/IslandApp/IslandApp.tsx
+++ packages/components/src/components/IslandApp/IslandApp.tsx
@@ -2,9 +2,5 @@ import React from 'react'
 import { Mapbox } from '../Mapbox'
 
 export const IslandApp = () => {
-  return (
-    <React.StrictMode>
-      <Mapbox />
-    </React.StrictMode>
-  )
+  return <Mapbox />
 }
