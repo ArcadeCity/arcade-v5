e69eeb8987023c6e39725da8c66393c014f772e5
Christopher David <chris@arcade.city>
2021-08-12 12:40:26 -0400

    Bitcoin Island text

--- packages/components/src/components/IslandApp/IslandApp.tsx
+++ packages/components/src/components/IslandApp/IslandApp.tsx
@@ -1,6 +1,35 @@
 import React from 'react'
+import { Text, View } from 'react-native'
+import { palette } from '../../theme'
 import { Mapbox } from '../Mapbox'
 
 export const IslandApp = () => {
-  return <Mapbox />
+  return (
+    <View style={{ flex: 1 }}>
+      <Mapbox />
+      <View
+        style={{
+          position: 'absolute',
+          top: 110,
+          width: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        pointerEvents='none'
+      >
+        <Text
+          style={{
+            fontFamily: 'Avenir',
+            fontSize: 74,
+            textAlign: 'center',
+            letterSpacing: 5,
+            lineHeight: 84,
+            color: palette.minsk,
+          }}
+        >
+          BITCOIN ISLAND
+        </Text>
+      </View>
+    </View>
+  )
 }
