bcb744d113c44ef1b60bee194361561841905bba
Christopher David <chris@arcade.city>
2021-08-12 12:08:36 -0400

    Builds with map

--- packages/components/src/components/Mapbox/Mapbox.tsx
+++ packages/components/src/components/Mapbox/Mapbox.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { View } from 'react-native'
+import MapboxGL from '@react-native-mapbox-gl/maps'
 import { palette } from '../../theme'
 
 export const Mapbox = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: palette.electricViolet }}></View>
+    <View style={{ flex: 1, backgroundColor: palette.electricViolet }}>
+      <MapboxGL.MapView style={{ flex: 1 }} />
+    </View>
   )
 }
