81770e30b8063b2788f6547411b8f81625be9ed6
Christopher David <chris@arcade.city>
2021-08-12 11:55:56 -0400

    Separate empty Mapbox component

--- packages/components/src/components/IslandApp/IslandApp.tsx
+++ packages/components/src/components/IslandApp/IslandApp.tsx
@@ -1,30 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import { Alert, View } from 'react-native'
-import { RootStore, RootStoreProvider, setupRootStore } from '../../stores'
-import { palette } from '../../theme'
+import React from 'react'
+import { Mapbox } from '../Mapbox'
 
 export const IslandApp = () => {
-  const [rootStore, setRootStore] = useState<RootStore | undefined>(undefined)
-
-  // Kick off initial async loading actions, like loading fonts and RootStore
-  useEffect(() => {
-    ;(async () => {
-      // Check if we're on the latest app version; download and refresh if not.
-      // await checkForUpdates()
-      // await initFonts() // expo
-      try {
-        setupRootStore(null).then(setRootStore)
-      } catch (e) {
-        Alert.alert('An error', e.message)
-      }
-    })()
-  }, [])
-
-  console.log('rootStore:', rootStore)
-
   return (
     <React.StrictMode>
-      <View style={{ flex: 1, backgroundColor: palette.electricIndigo }}></View>
+      <Mapbox />
     </React.StrictMode>
   )
 }
---
+++ packages/components/src/components/Mapbox/Mapbox.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { View } from 'react-native'
+import { palette } from '../../theme'
+
+export const Mapbox = () => {
+  return (
+    <View style={{ flex: 1, backgroundColor: palette.electricViolet }}></View>
+  )
+}
