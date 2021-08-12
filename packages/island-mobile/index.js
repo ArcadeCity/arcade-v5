56c1326049f51823a548019e108cdac53e8900dd
Christopher David <chris@arcade.city>
2021-08-12 11:07:14 -0400

    Renaming stuff same as city-mobile

--- packages/island-mobile/index.js
+++ packages/island-mobile/index.js
@@ -1,9 +1,6 @@
-/**
- * @format
- */
+import { AppRegistry } from 'react-native'
+import { name as appName } from './app.json'
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const { CityApp } = require('@arcadecity/components/src/components/CityApp')
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => CityApp)
