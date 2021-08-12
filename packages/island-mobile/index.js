77bc5b1a144f54ec1400e8bba792faf1aa0b515f
Christopher David <chris@arcade.city>
2021-08-12 11:36:17 -0400

    [island-mobile] Repeat RN setup for Bitcoin Island mobile app (#10)
    * README
    
    * npx react-native init island
    
    * Renaming stuff same as city-mobile
    
    * nodemodules path etc
    
    * BitcoinIsland runs on iOS device
    
    * island builds on emulator
    
    * and works on mobile

---
+++ packages/island-mobile/index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native'
+import { name as appName } from './app.json'
+
+const { CityApp } = require('@arcadecity/components/src/components/CityApp')
+
+AppRegistry.registerComponent(appName, () => CityApp)
