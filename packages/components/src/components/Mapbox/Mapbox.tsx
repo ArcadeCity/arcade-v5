a6975d39aa14022f1b335be8d5e9a00c5b8597ef
Christopher David <chris@arcade.city>
2021-08-12 12:11:52 -0400

    Add access token - map renders

--- packages/components/src/components/Mapbox/Mapbox.tsx
+++ packages/components/src/components/Mapbox/Mapbox.tsx
@@ -3,6 +3,10 @@ import { View } from 'react-native'
 import MapboxGL from '@react-native-mapbox-gl/maps'
 import { palette } from '../../theme'
 
+MapboxGL.setAccessToken(
+  'pk.eyJ1IjoiYWNsaW9ucyIsImEiOiJja3M5NGNvOHYwcjJ4MnZwZW10azYyNDFhIn0.nKlBGTV6rb6UbK53mDZDWQ'
+)
+
 export const Mapbox = () => {
   return (
     <View style={{ flex: 1, backgroundColor: palette.electricViolet }}>
