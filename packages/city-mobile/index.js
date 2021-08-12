4b68fd539bb8099c9a2c6e9e4828075babb02df6
Christopher David <chris@arcade.city>
2021-08-12 09:41:53 -0400

    Load the CityApp component

--- packages/city-mobile/index.js
+++ packages/city-mobile/index.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native'
-import App from './App'
 import { name as appName } from './app.json'
 
-AppRegistry.registerComponent(appName, () => App)
+const { CityApp } = require('@arcadecity/components/src/components/CityApp')
+
+AppRegistry.registerComponent(appName, () => CityApp)
