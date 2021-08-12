7dab518a7738ed37b671d207e0d784aca41231d2
Christopher David <chris@arcade.city>
2021-08-12 19:27:25 -0400

    axe old ceramicTest - tsc 80

--- packages/components/src/views/wallet/screens/placeholder/placeholder.tsx
+++ packages/components/src/views/wallet/screens/placeholder/placeholder.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { useStores } from 'stores'
 import { Button, Screen, Text } from 'views/shared'
-import { ceramicTest } from './ceramicTest'
 
 export const Placeholder = () => {
   const { setOptions } = useNavigation()
@@ -21,11 +20,6 @@ export const Placeholder = () => {
         text='Init wallet'
         style={{ marginVertical: 35 }}
         onPress={walletStore.init}
-      />
-      <Button
-        text='Ceramic test'
-        style={{ marginVertical: 35 }}
-        onPress={ceramicTest}
       />
       <Button text='Give me sats' style={{ marginTop: 35 }} />
     </Screen>
