254cda25b781222ebf19de9aa4df5d6b3962c219
Christopher David <chris@arcade.city>
2021-08-13 18:29:16 -0400

    alias seems to work for i18n

--- packages/city-mobile/babel.config.js
+++ packages/city-mobile/babel.config.js
@@ -1,3 +1,33 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['../components/src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+          '.stories.tsx',
+        ],
+        alias: {
+          app: './src/app',
+          i18n: '../components/src/i18n',
+          lib: './src/lib',
+          navigation: './src/navigation',
+          services: './src/services',
+          stores: './src/stores',
+          storybook: './storybook',
+          views: './src/views',
+        },
+      },
+    ],
+  ],
 }
