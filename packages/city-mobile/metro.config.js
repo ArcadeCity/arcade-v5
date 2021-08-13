17d356648b19cf7e9800c8908750c4f4f2a46921
Christopher David <chris@arcade.city>
2021-08-13 10:49:14 -0400

    expo-ize metro config

--- packages/city-mobile/metro.config.js
+++ packages/city-mobile/metro.config.js
@@ -1,14 +1,18 @@
 const path = require('path')
 
-module.exports = {
-  watchFolders: [path.resolve(__dirname, '../../')],
-  transformer: {
-    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
+const { getDefaultConfig } = require('expo/metro-config')
+
+const config = getDefaultConfig(__dirname)
+
+config.watchFolders = [path.resolve(__dirname, '../../')]
+config.transformer.assetPlugins = ['expo-asset/tools/hashAssetFiles']
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: true,
   },
-}
+})
+config.resolver.assetExts.push('fbx', 'glb', 'obj')
+config.resolver.sourceExts.push('cjs', 'properties', 'expo.js')
+
+module.exports = config
