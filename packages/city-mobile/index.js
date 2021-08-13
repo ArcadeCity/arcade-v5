df627781cca7fb3cd28cd0f9ec062695f3bf41f5
Christopher David <chris@arcade.city>
2021-08-12 23:43:22 -0400

    gim dat shim

--- packages/city-mobile/index.js
+++ packages/city-mobile/index.js
@@ -2,5 +2,6 @@ import { AppRegistry } from 'react-native'
 import { name as appName } from './app.json'
 
 const { CityApp } = require('@arcadecity/components/src/components/CityApp')
+require('@arcadecity/components/shim')
 
 AppRegistry.registerComponent(appName, () => CityApp)
