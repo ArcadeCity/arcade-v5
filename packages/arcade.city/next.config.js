3935436557139b546877a74ca33f765f63781795
Christopher David <chris@arcade.city>
2021-08-14 16:08:32 -0400

    model is here thank you lerp

--- packages/arcade.city/next.config.js
+++ packages/arcade.city/next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      // Transform all direct `react-native` imports to `react-native-web`
+      'react-native$': 'react-native-web',
+    }
+    config.resolve.extensions = [
+      '.web.js',
+      '.web.ts',
+      '.web.tsx',
+      ...config.resolve.extensions,
+    ]
+    return config
+  },
 }
--- packages/arcade.city/pages/index.tsx
+++ packages/arcade.city/pages/index.tsx
@@ -3,6 +3,9 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { Canvas, useFrame } from '@react-three/fiber'
+import { useGLTF } from '@react-three/drei'
+import lerp from 'lerp'
+// import Model from '../components/Model'
 // import { Canvas } from '../components/Canvas'
 
 function Box(props) {
@@ -55,6 +58,7 @@ const Home: NextPage = () => {
           <pointLight position={[0, 10, -10]} intensity={7} />
           <Box position={[-1.2, 0, 0]} />
           <Box position={[1.2, 0, 0]} />
+          <Model position={[0, -6, 0]} rotation={[0, -0.2, 0]} />
         </Canvas>
       </div>
 
@@ -74,3 +78,89 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+function Model(props) {
+  const group = useRef()
+  const { nodes } = useGLTF('/graces-draco.glb')
+
+  useFrame(({ clock, mouse }) => {
+    group.current.rotation.y = lerp(
+      group.current.rotation.y,
+      mouse.x * (Math.PI / 5),
+      0.005
+    )
+  })
+
+  console.log('nodes:', nodes)
+
+  if (!nodes) return <></>
+
+  return (
+    <group {...props} dispose={null}>
+      <group ref={group}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Node_3.geometry}
+          rotation={[-Math.PI / 2, 0, 0]}
+          scale={[0.2, 0.224, 0.224]}
+        >
+          <meshStandardMaterial
+            attach='material'
+            roughness={0.9}
+            metalness={0.5}
+            color='#474747'
+          />
+        </mesh>
+        <Lights />
+      </group>
+    </group>
+  )
+}
+
+function Lights() {
+  const groupL = useRef()
+  const groupR = useRef()
+  const front = useRef()
+
+  useFrame(({ clock, mouse }) => {
+    groupL.current.rotation.y = lerp(
+      groupL.current.rotation.y,
+      -mouse.x * (Math.PI / 2),
+      0.1
+    )
+    groupR.current.rotation.y = lerp(
+      groupR.current.rotation.y,
+      mouse.x * (Math.PI / 2),
+      0.1
+    )
+    front.current.position.x = lerp(front.current.position.x, mouse.x * 12, 0.4)
+    front.current.position.y = lerp(
+      front.current.position.y,
+      7 + mouse.y * 4,
+      0.4
+    )
+  })
+
+  return (
+    <>
+      <group ref={groupL}>
+        <pointLight position={[0, 7, -15]} distance={15} intensity={10} />
+      </group>
+      <group ref={groupR}>
+        <pointLight position={[0, 7, -15]} distance={15} intensity={10} />
+      </group>
+      <spotLight
+        castShadow
+        ref={front}
+        penumbra={1}
+        angle={Math.PI / 3}
+        position={[0, 0, 8]}
+        distance={11}
+        intensity={8}
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
+      />
+    </>
+  )
+}
