ba6031fbd6715e39a25f1aef991e58c1be7567b0
Christopher David <chris@arcade.city>
2021-08-15 13:27:06 -0400

    animate the light

--- packages/arcade.city/components/LCanvas.tsx
+++ packages/arcade.city/components/LCanvas.tsx
@@ -3,12 +3,19 @@ import { Canvas } from '@react-three/fiber'
 import Box from './Box'
 import { useRouter } from 'next/router'
 import { Html, Loader } from '@react-three/drei'
+import { useSpring } from '@react-spring/core'
+import { a } from '@react-spring/three'
 import Model from './Model'
 
 const LCanvas = () => {
   const router = useRouter()
   console.log(router)
-  const intensity = router.pathname === '/' ? 1 : 5
+  const location = router.pathname
+  // const intensity = router.pathname === '/' ? 1 : 5
+  const props = useSpring({
+    intensity: location === '/' ? 1 : 15,
+  })
+  console.log(props.intensity)
   return (
     <>
       <div
@@ -25,7 +32,7 @@ const LCanvas = () => {
           camera={{ position: [0, 0, 12], fov: 50, near: 7, far: 15 }}
         >
           <fog attach='fog' args={['black', 0, 20]} />
-          <pointLight position={[0, 10, -10]} intensity={intensity} />
+          <a.pointLight position={[0, 10, -10]} intensity={props.intensity} />
           {/* <Box /> */}
           <Suspense fallback={<Html center className='loader'></Html>}>
             <Model position={[0, -6, 0]} rotation={[0, -0.2, 0]} />
