8f5350dbfd14434976acb62eda1e1e352b48b962
Christopher David <chris@arcade.city>
2021-08-15 13:13:55 -0400

    Re-add loader

--- packages/arcade.city/components/LCanvas.tsx
+++ packages/arcade.city/components/LCanvas.tsx
@@ -6,27 +6,30 @@ import Model from './Model'
 
 const LCanvas = ({ children }: any) => {
   return (
-    <div
-      style={{
-        position: 'absolute',
-        flex: 1,
-        width: '100vw',
-        height: '100vh',
-        zIndex: 0,
-      }}
-    >
-      <Canvas
-        shadows
-        camera={{ position: [0, 0, 12], fov: 50, near: 7, far: 15 }}
+    <>
+      <div
+        style={{
+          position: 'absolute',
+          flex: 1,
+          width: '100vw',
+          height: '100vh',
+          zIndex: 0,
+        }}
       >
-        <fog attach='fog' args={['black', 0, 20]} />
-        <pointLight position={[0, 10, -10]} intensity={1} />
-        {/* <Box /> */}
-        <Suspense fallback={<Html center className='loader'></Html>}>
-          <Model position={[0, -6, 0]} rotation={[0, -0.2, 0]} />
-        </Suspense>
-      </Canvas>
-    </div>
+        <Canvas
+          shadows
+          camera={{ position: [0, 0, 12], fov: 50, near: 7, far: 15 }}
+        >
+          <fog attach='fog' args={['black', 0, 20]} />
+          <pointLight position={[0, 10, -10]} intensity={1} />
+          {/* <Box /> */}
+          <Suspense fallback={<Html center className='loader'></Html>}>
+            <Model position={[0, -6, 0]} rotation={[0, -0.2, 0]} />
+          </Suspense>
+        </Canvas>
+      </div>
+      <Loader />
+    </>
   )
 }
 
