ca73541fad0b4f0c06659d5bf3b5fa3d8dcf9a8f
Christopher David <chris@arcade.city>
2021-08-15 16:42:20 -0400

    navbar changes underline on location

--- packages/arcade.city/components/Navbar.tsx
+++ packages/arcade.city/components/Navbar.tsx
@@ -1,14 +1,19 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-export const Navbar = () => (
-  <div className={styles.header}>
-    <span className={styles.active}>
-      <Link href='/'>HOME</Link>
-    </span>
+export const Navbar = () => {
+  const router = useRouter()
+  const location = router.pathname
+  return (
+    <div className={styles.header}>
+      <span className={location === '/' ? styles.active : ''}>
+        <Link href='/'>HOME</Link>
+      </span>
 
-    <span>
-      <Link href='/city'>CITY</Link>
-    </span>
-  </div>
-)
+      <span className={location === '/city' ? styles.active : ''}>
+        <Link href='/city'>CITY</Link>
+      </span>
+    </div>
+  )
+}
