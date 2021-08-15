9d3bbc2e3c2c95201d356fb44a5d96f2b000ceb5
Christopher David <chris@arcade.city>
2021-08-15 16:33:15 -0400

    Ah yes we can pull in CityWeb

--- packages/arcade.city/pages/city.tsx
+++ packages/arcade.city/pages/city.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { Button } from 'react-native'
-import { Text } from '../../components/src/views/shared'
+import { CityWeb } from '../../components/src/components/CityWeb'
 
 const Home: NextPage = () => {
   return (
@@ -36,7 +36,7 @@ const Home: NextPage = () => {
             console.log('ok')
           }}
         />
-        <Text text='Any chance this works?' />
+        <CityWeb />
       </main>
     </div>
   )
