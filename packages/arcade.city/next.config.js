712b26c54862727849a406eb93efb8da544ee368
Christopher David <chris@arcade.city>
2021-08-16 11:36:00 -0400

    MDX config

--- packages/arcade.city/next.config.js
+++ packages/arcade.city/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+const { createLoader } = require('simple-functional-loader')
+const withSyntaxHighlighting = require('./remark/withSyntaxHighlighting')
+const withProse = require('./remark/withProse')
+
 module.exports = {
   // reactStrictMode: true,
   experimental: {
@@ -10,7 +14,7 @@ module.exports = {
     disableStaticImages: true,
   },
 
-  webpack: (config) => {
+  webpack: (config, options) => {
     config.module.rules.push({
       test: /\.(svg|png|jpe?g|gif|mp4)$/i,
       use: [
@@ -20,6 +24,70 @@ module.exports = {
             publicPath: '/_next',
             name: 'static/media/[name].[hash].[ext]',
           },
+        },
+      ],
+    })
+
+    const mdx = [
+      options.defaultLoaders.babel,
+      {
+        loader: '@mdx-js/loader',
+        options: {
+          remarkPlugins: [withProse, withSyntaxHighlighting],
+        },
+      },
+    ]
+
+    config.module.rules.push({
+      test: /\.mdx$/,
+      oneOf: [
+        {
+          test: /snippets/,
+          use: mdx,
+        },
+        {
+          resourceQuery: /preview/,
+          use: [
+            ...mdx,
+            createLoader(function (src) {
+              if (src.includes('<!--more-->')) {
+                const [preview] = src.split('<!--more-->')
+                return this.callback(null, preview)
+              }
+
+              const [preview] = src.split('<!--/excerpt-->')
+              return this.callback(null, preview.replace('<!--excerpt-->', ''))
+            }),
+          ],
+        },
+        {
+          resourceQuery: /rss/,
+          use: mdx,
+        },
+        {
+          use: [
+            ...mdx,
+            createLoader(function (src) {
+              const content = [
+                'import Post from "@/components/Post"',
+                'export { getStaticProps } from "@/getStaticProps"',
+                src,
+                'export default Post',
+              ].join('\n')
+
+              if (content.includes('<!--more-->')) {
+                return this.callback(
+                  null,
+                  content.split('<!--more-->').join('\n')
+                )
+              }
+
+              return this.callback(
+                null,
+                content.replace(/<!--excerpt-->.*<!--\/excerpt-->/s, '')
+              )
+            }),
+          ],
         },
       ],
     })
