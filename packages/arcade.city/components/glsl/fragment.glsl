69c127cdb16b19553fc9dce415d28e6f9a1110ee
Christopher David <chris@arcade.city>
2021-08-17 10:41:18 -0400

    add initial atmosphere fragment

--- packages/arcade.city/components/glsl/fragment.glsl
+++ packages/arcade.city/components/glsl/fragment.glsl
@@ -1,6 +1,9 @@
 uniform sampler2D globeTexture;
 varying vec2 vertexUV;
+varying vec3 vertexNormal;
 
 void main() {
-  gl_FragColor = vec4(vec3(0, 0, 0.2) + texture2D(globeTexture, vertexUV).xyz, 1.0);
+  float intensity = 1.05 - dot(vertexNormal, vec3(0, 0, 1.0));
+  vec3 atmosphere = vec3(0.3, 0.6, 1.0) * pow(intensity, 1.5);
+  gl_FragColor = vec4(atmosphere + texture2D(globeTexture, vertexUV).xyz, 1.0);
 }
--- packages/arcade.city/components/glsl/vertex.glsl
+++ packages/arcade.city/components/glsl/vertex.glsl
@@ -1,6 +1,8 @@
 varying vec2 vertexUV;
+varying vec3 vertexNormal;
 
 void main() {
   vertexUV = uv;
+  vertexNormal = normal;
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
