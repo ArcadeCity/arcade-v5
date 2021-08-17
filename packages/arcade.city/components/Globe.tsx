c66129633a462ff175680c1ea1417b8070dd41dc
Christopher David <chris@arcade.city>
2021-08-17 10:12:37 -0400

    sexy

--- packages/arcade.city/components/Globe.tsx
+++ packages/arcade.city/components/Globe.tsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react'
 import { useTexture } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
+import fragmentShader from './glsl/fragment.glsl'
 import lerp from 'lerp'
 
 export const Globe = () => {
@@ -38,34 +39,34 @@ export const Globe = () => {
   )
 }
 
-const fragmentShader = `
-    varying vec3 Normal;
-    varying vec3 Position;
+// const fragmentShader = `
+//     varying vec3 Normal;
+//     varying vec3 Position;
 
-    uniform vec3 Ka;
-    uniform vec3 Kd;
-    uniform vec3 Ks;
-    uniform vec4 LightPosition;
-    uniform vec3 LightIntensity;
-    uniform float Shininess;
+//     uniform vec3 Ka;
+//     uniform vec3 Kd;
+//     uniform vec3 Ks;
+//     uniform vec4 LightPosition;
+//     uniform vec3 LightIntensity;
+//     uniform float Shininess;
 
-    vec3 phong() {
-      vec3 n = normalize(Normal);
-      vec3 s = normalize(vec3(LightPosition) - Position);
-      vec3 v = normalize(vec3(-Position));
-      vec3 r = reflect(-s, n);
+//     vec3 phong() {
+//       vec3 n = normalize(Normal);
+//       vec3 s = normalize(vec3(LightPosition) - Position);
+//       vec3 v = normalize(vec3(-Position));
+//       vec3 r = reflect(-s, n);
 
-      vec3 ambient = Ka;
-      vec3 diffuse = Kd * max(dot(s, n), 0.0);
-      vec3 specular = Ks * pow(max(dot(r, v), 0.0), Shininess);
+//       vec3 ambient = Ka;
+//       vec3 diffuse = Kd * max(dot(s, n), 0.0);
+//       vec3 specular = Ks * pow(max(dot(r, v), 0.0), Shininess);
 
-      return LightIntensity * (ambient + diffuse + specular);
-    }
+//       return LightIntensity * (ambient + diffuse + specular);
+//     }
 
-    void main() {
-      vec3 blue = vec3(0.0, 0.0, 1.0);
-      gl_FragColor = vec4(blue*phong(), 1.0);
-  }`
+//     void main() {
+//       vec3 blue = vec3(0.0, 0.0, 1.0);
+//       gl_FragColor = vec4(blue*phong(), 1.0);
+//   }`
 const vertexShader = `
     varying vec3 Normal;
     varying vec3 Position;
