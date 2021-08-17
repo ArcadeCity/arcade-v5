81fce3e4e00eb35958f876b9cf72809525f2a698
Christopher David <chris@arcade.city>
2021-08-17 10:34:06 -0400

    add tint

--- packages/arcade.city/components/glsl/fragment.glsl
+++ packages/arcade.city/components/glsl/fragment.glsl
@@ -2,5 +2,5 @@ uniform sampler2D globeTexture;
 varying vec2 vertexUV;
 
 void main() {
-  gl_FragColor = texture2D(globeTexture, vertexUV);
+  gl_FragColor = vec4(vec3(0, 0, 0.2) + texture2D(globeTexture, vertexUV).xyz, 1.0);
 }
