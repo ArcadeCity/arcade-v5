f1089f7d85d42eb8912dd6821825f4ffc91c750d
Christopher David <chris@arcade.city>
2021-08-19 18:50:23 -0400

    blog container nav

--- packages/arcade.city/pages/blog/index.tsx
+++ packages/arcade.city/pages/blog/index.tsx
@@ -1,9 +1,8 @@
 import type { NextPage } from 'next'
-// import styles from '../../styles/Home.module.css'
 import { Navbar } from '../../components/Navbar'
 import Link from 'next/link'
 import Image from 'next/image'
-// import image from './bitcoin-first/card.jpg'
+import imgBitcoin from './bitcoin-first/card.jpg'
 import image from './antidote/authoritarian2.png'
 
 const Blog: NextPage = () => {
@@ -12,18 +11,58 @@ const Blog: NextPage = () => {
       <div className='absolute w-full h-screen px-4 sm:px-6 lg:px-8 py-16 overflow-y-auto'>
         <Navbar />
         <div className='mt-24 max-w-3xl mx-auto'>
-          <div className='mt-12 max-w-lg mx-auto grid gap-8 lg:grid-cols-2 lg:max-w-none'>
-            <Link href='/blog/antidote' passHref>
-              <div className='shadow-xl cursor-pointer flex flex-col rounded-lg overflow-hidden border-2 border-white'>
+          <div className='mt-12 w-lg max-w-lg mx-auto grid gap-8 lg:grid-cols-2 lg:max-w-none'>
+            <Link href='/blog/bitcoin-first' passHref>
+              <div
+                style={{ maxWidth: 500 }}
+                className='shadow-xl cursor-pointer flex flex-col rounded-lg overflow-hidden border-2 border-white'
+              >
                 <div className='flex-shrink-0 bg-purple'>
-                  <Image
-                    className='h-48 w-full object-cover object-center'
-                    src={image}
-                    alt=''
-                    width='100'
-                    height='50'
-                    layout='responsive'
-                  />
+                  <div style={IMGCONTAINER}>
+                    <Image
+                      className='h-48 w-full object-cover object-center'
+                      src={imgBitcoin}
+                      alt=''
+                      layout='fill'
+                      objectFit='cover'
+                    />
+                  </div>
+                </div>
+
+                <div className='flex-1 bg-white p-6 flex flex-col justify-between'>
+                  <div className='flex-1'>
+                    <div className='block mt-1'>
+                      <p className='text-xl font-semibold text-gray-900'>
+                        Bitcoin First
+                      </p>
+                      <div className='mt-1 flex space-x-1 text-sm text-gray-900'>
+                        <time dateTime='2021-08-01'>August 19, 2021</time>
+                      </div>
+                      <p className='mt-4 text-base text-gray-900'>
+                        We are pivoting to build on Bitcoin. All serious
+                        builders should do the same.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Link>
+
+            <Link href='/blog/antidote' passHref>
+              <div
+                style={{ maxWidth: 500 }}
+                className='shadow-xl cursor-pointer flex flex-col rounded-lg overflow-hidden border-2 border-white'
+              >
+                <div className='flex-shrink-0 bg-purple'>
+                  <div style={IMGCONTAINER}>
+                    <Image
+                      className='h-48 w-full object-cover object-center'
+                      src={image}
+                      alt=''
+                      layout='fill'
+                      objectFit='cover'
+                    />
+                  </div>
                 </div>
 
                 <div className='flex-1 bg-white p-6 flex flex-col justify-between'>
@@ -60,3 +99,9 @@ const Blog: NextPage = () => {
 }
 
 export default Blog
+
+const IMGCONTAINER: any = {
+  width: '100%',
+  height: 250,
+  position: 'relative',
+}
