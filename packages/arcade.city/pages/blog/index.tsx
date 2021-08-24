c41ebf84998eff225ad247c4feb1cbc20fc52f32
Christopher David <chris@arcade.city>
2021-08-24 15:30:36 -0500

    [arcade.city] Add blog post "OnlySats" (#26)
    * start blog post
    
    * Copy about done

--- packages/arcade.city/pages/blog/index.tsx
+++ packages/arcade.city/pages/blog/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import { Navbar } from '../../components/Navbar'
 import Link from 'next/link'
 import Image from 'next/image'
+import imgOnlysats from './onlysats/onlysats3.png'
 import imgBitcoin from './bitcoin-first/bitcoin.png'
 import image from './antidote/authoritarian2.png'
 
@@ -12,6 +13,42 @@ const Blog: NextPage = () => {
         <Navbar />
         <div className='mt-24 max-w-3xl mx-auto'>
           <div className='mt-12 w-lg max-w-lg mx-auto grid gap-8 lg:grid-cols-2 lg:max-w-none'>
+            <Link href='/blog/onlysats' passHref>
+              <div
+                style={{ minWidth: 500, maxWidth: 500 }}
+                className='shadow-xl cursor-pointer flex flex-col rounded-lg overflow-hidden border-2 border-white'
+              >
+                <div className='flex-shrink-0 bg-purple'>
+                  <div style={IMGCONTAINER}>
+                    <Image
+                      className='h-48 w-full object-cover object-top'
+                      src={imgOnlysats}
+                      alt=''
+                      layout='fill'
+                      objectFit='cover'
+                    />
+                  </div>
+                </div>
+
+                <div className='flex-1 bg-white p-6 flex flex-col justify-between'>
+                  <div className='flex-1'>
+                    <div className='block mt-1'>
+                      <p className='text-xl font-semibold text-gray-900'>
+                        OnlySats
+                      </p>
+                      <div className='mt-1 flex space-x-1 text-sm text-gray-900'>
+                        <time dateTime='2021-08-24'>August 24, 2021</time>
+                      </div>
+                      <p className='mt-4 text-base text-gray-900'>
+                        Sex workers deserve to make a stable living without fear
+                        of deplatforming. Bitcoin fixes this.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Link>
+
             <Link href='/blog/bitcoin-first' passHref>
               <div
                 style={{ maxWidth: 500 }}
