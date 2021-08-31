1e8d19e59918599f398e50363fadad36c7167f53
Christopher David <chris@arcade.city>
2021-08-31 17:09:54 -0500

    [explorer] Setup with harp.gl CRA example (#27)
    * Pull in harp-gl example as explorer
    
    * Update scripts
    
    * temporarily re-add stupid package-lock
    
    * add env to gitignore
    
    * update dev script

---
+++ packages/explorer/src/App.js
@@ -0,0 +1,100 @@
+import React, { useEffect, useRef } from 'react';
+
+import { GeoCoordinates } from '@here/harp-geoutils';
+import { GeoJsonDataProvider } from '@here/harp-geojson-datasource';
+import { MapControls, MapControlsUI } from '@here/harp-map-controls';
+import { CopyrightElementHandler, MapView } from '@here/harp-mapview';
+import { APIFormat, AuthenticationMethod, OmvDataSource } from '@here/harp-omv-datasource';
+import { apiKey, theme, copyrightInfo } from './config';
+
+const initialCoordinates = new GeoCoordinates(52.53102, 13.3848);
+const initialZoomLevel = 5;
+const minZoomLevel = 3;
+const maxZoomLevel = 10;
+
+const geoJsonDataProvider = new GeoJsonDataProvider('offices', new URL('/resources/offices.json', window.location.href));
+const geoJsonDataSource = new OmvDataSource({ name: 'offices', dataProvider: geoJsonDataProvider });
+
+const baseMap = new OmvDataSource({
+  baseUrl: 'https://vector.hereapi.com/v2/vectortiles/base/mc',
+  apiFormat: APIFormat.XYZOMV,
+  styleSetName: 'tilezen',
+  authenticationCode: apiKey,
+  authenticationMethod: {
+    method: AuthenticationMethod.QueryString,
+    name: 'apikey',
+  },
+  copyrightInfo,
+});
+
+const styleSet = [
+  {
+    when: '$geometryType == "point"',
+    technique: 'circles',
+    attr: {
+      color: '#01a39c',
+      size: 48,
+      renderOrder: 100,
+    },
+  },
+  {
+    when: '$geometryType == "line"',
+    technique: 'solid-line',
+    attr: {
+      color: '#f8bc02',
+      lineWidth: 10,
+      metricUnit: 'Pixel',
+      renderOrder: 100,
+    },
+  },
+];
+
+const App = () => {
+  const canvasRef = useRef(null);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    const map = mapRef.current = new MapView({
+      theme,
+      canvas: canvasRef.current,
+      target: initialCoordinates,
+      zoomLevel: initialZoomLevel,
+    });
+
+    map.addDataSource(baseMap);
+    map.addDataSource(geoJsonDataSource).then(() => geoJsonDataSource.setStyleSet(styleSet));
+
+    const onWindowResize = () => map.resize(window.innerWidth, window.innerHeight);
+    window.addEventListener('resize', onWindowResize);
+
+    return () => window.removeEventListener('resize', onWindowResize);
+  }, []);
+
+  useEffect(() => {
+    const controls = new MapControls(mapRef.current);
+    const uiControls = new MapControlsUI(controls);
+
+    controls.minZoomLevel = minZoomLevel;
+    controls.maxZoomLevel = maxZoomLevel;
+
+    canvasRef.current
+      .parentElement
+      .appendChild(uiControls.domElement);
+
+    CopyrightElementHandler
+      .install('copyright-notice')
+      .attach(mapRef.current);
+  }, []);
+
+  return (
+    <div className="app">
+      <canvas ref={canvasRef} />
+      <div id="copyright-notice"></div>
+      <div className="description">
+        HERE Office Locations <br /> Around the World
+      </div>
+    </div>
+  );
+};
+
+export default App;
