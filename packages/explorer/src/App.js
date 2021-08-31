d1c41ad44dbbebb415a4bb63b026c3ca90d920f7
Christopher David <chris@arcade.city>
2021-08-31 17:42:43 -0500

    Axe dataset

--- packages/explorer/src/App.js
+++ packages/explorer/src/App.js
@@ -1,19 +1,29 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react'
 
-import { GeoCoordinates } from '@here/harp-geoutils';
-import { GeoJsonDataProvider } from '@here/harp-geojson-datasource';
-import { MapControls, MapControlsUI } from '@here/harp-map-controls';
-import { CopyrightElementHandler, MapView } from '@here/harp-mapview';
-import { APIFormat, AuthenticationMethod, OmvDataSource } from '@here/harp-omv-datasource';
-import { apiKey, theme, copyrightInfo } from './config';
+import { GeoCoordinates } from '@here/harp-geoutils'
+import { GeoJsonDataProvider } from '@here/harp-geojson-datasource'
+import { MapControls, MapControlsUI } from '@here/harp-map-controls'
+import { CopyrightElementHandler, MapView } from '@here/harp-mapview'
+import {
+  APIFormat,
+  AuthenticationMethod,
+  OmvDataSource,
+} from '@here/harp-omv-datasource'
+import { apiKey, theme, copyrightInfo } from './config'
 
-const initialCoordinates = new GeoCoordinates(52.53102, 13.3848);
-const initialZoomLevel = 5;
-const minZoomLevel = 3;
-const maxZoomLevel = 10;
+const initialCoordinates = new GeoCoordinates(0, 13.3848)
+const initialZoomLevel = 2.5
+const minZoomLevel = 2.5
+const maxZoomLevel = 10
 
-const geoJsonDataProvider = new GeoJsonDataProvider('offices', new URL('/resources/offices.json', window.location.href));
-const geoJsonDataSource = new OmvDataSource({ name: 'offices', dataProvider: geoJsonDataProvider });
+const geoJsonDataProvider = new GeoJsonDataProvider(
+  'offices',
+  new URL('/resources/offices.json', window.location.href)
+)
+const geoJsonDataSource = new OmvDataSource({
+  name: 'offices',
+  dataProvider: geoJsonDataProvider,
+})
 
 const baseMap = new OmvDataSource({
   baseUrl: 'https://vector.hereapi.com/v2/vectortiles/base/mc',
@@ -25,7 +35,7 @@ const baseMap = new OmvDataSource({
     name: 'apikey',
   },
   copyrightInfo,
-});
+})
 
 const styleSet = [
   {
@@ -47,54 +57,51 @@ const styleSet = [
       renderOrder: 100,
     },
   },
-];
+]
 
 const App = () => {
-  const canvasRef = useRef(null);
-  const mapRef = useRef(null);
+  const canvasRef = useRef(null)
+  const mapRef = useRef(null)
 
   useEffect(() => {
-    const map = mapRef.current = new MapView({
+    const map = (mapRef.current = new MapView({
       theme,
       canvas: canvasRef.current,
       target: initialCoordinates,
       zoomLevel: initialZoomLevel,
-    });
+    }))
 
-    map.addDataSource(baseMap);
-    map.addDataSource(geoJsonDataSource).then(() => geoJsonDataSource.setStyleSet(styleSet));
+    map.addDataSource(baseMap)
+    // map
+    //   .addDataSource(geoJsonDataSource)
+    //   .then(() => geoJsonDataSource.setStyleSet(styleSet))
 
-    const onWindowResize = () => map.resize(window.innerWidth, window.innerHeight);
-    window.addEventListener('resize', onWindowResize);
+    const onWindowResize = () =>
+      map.resize(window.innerWidth, window.innerHeight)
+    window.addEventListener('resize', onWindowResize)
 
-    return () => window.removeEventListener('resize', onWindowResize);
-  }, []);
+    return () => window.removeEventListener('resize', onWindowResize)
+  }, [])
 
   useEffect(() => {
-    const controls = new MapControls(mapRef.current);
-    const uiControls = new MapControlsUI(controls);
+    const controls = new MapControls(mapRef.current)
+    const uiControls = new MapControlsUI(controls)
 
-    controls.minZoomLevel = minZoomLevel;
-    controls.maxZoomLevel = maxZoomLevel;
+    controls.minZoomLevel = minZoomLevel
+    controls.maxZoomLevel = maxZoomLevel
 
-    canvasRef.current
-      .parentElement
-      .appendChild(uiControls.domElement);
+    canvasRef.current.parentElement.appendChild(uiControls.domElement)
 
-    CopyrightElementHandler
-      .install('copyright-notice')
-      .attach(mapRef.current);
-  }, []);
+    // CopyrightElementHandler.install('copyright-notice').attach(mapRef.current)
+  }, [])
 
   return (
-    <div className="app">
+    <div className='app'>
       <canvas ref={canvasRef} />
-      <div id="copyright-notice"></div>
-      <div className="description">
-        HERE Office Locations <br /> Around the World
-      </div>
+      {/* <div id='copyright-notice'></div> */}
+      {/* <div className='description'>Arcade Nodes</div> */}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
