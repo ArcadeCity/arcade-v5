4f179aefb538ae0e6bc1ab6abb4c533e2a3858ff
Christopher David <chris@arcade.city>
2021-08-31 18:06:37 -0500

    Hide text

--- packages/explorer/src/App.js
+++ packages/explorer/src/App.js
@@ -70,6 +70,7 @@ const App = () => {
       zoomLevel: initialZoomLevel,
     }))
 
+    map.renderLabels = false
     map.addDataSource(baseMap)
     // map
     //   .addDataSource(geoJsonDataSource)
