6677418ac74f983100fc45a302a250232cbe61d4
Christopher David <chris@arcade.city>
2021-09-05 11:37:18 -0500

    Pull nostr-tools into lib and format

---
+++ packages/components/src/lib/nostr-tools/event.js
@@ -0,0 +1,42 @@
+import * as secp256k1 from 'noble-secp256k1'
+
+import { sha256 } from './utils'
+
+export function getBlankEvent() {
+  return {
+    kind: 255,
+    pubkey: null,
+    content: '',
+    tags: [],
+    created_at: 0,
+  }
+}
+
+export function serializeEvent(evt) {
+  return JSON.stringify([
+    0,
+    evt.pubkey,
+    evt.created_at,
+    evt.kind,
+    evt.tags || [],
+    evt.content,
+  ])
+}
+
+export async function getEventHash(event) {
+  let eventHash = await sha256(Buffer.from(serializeEvent(event)))
+  return Buffer.from(eventHash).toString('hex')
+}
+
+export async function verifySignature(event) {
+  return await secp256k1.schnorr.verify(
+    event.sig,
+    await getEventHash(event),
+    event.pubkey
+  )
+}
+
+export async function signEvent(event, key) {
+  let eventHash = await getEventHash(event)
+  return await secp256k1.schnorr.sign(eventHash, key)
+}
---
+++ packages/components/src/lib/nostr-tools/index.js
@@ -0,0 +1,23 @@
+import { relayConnect } from './relay'
+import { relayPool } from './pool'
+import {
+  getBlankEvent,
+  signEvent,
+  verifySignature,
+  serializeEvent,
+  getEventHash,
+} from './event'
+import { makeRandom32, sha256, getPublicKey } from './utils'
+
+export {
+  relayConnect,
+  relayPool,
+  signEvent,
+  verifySignature,
+  serializeEvent,
+  getEventHash,
+  makeRandom32,
+  sha256,
+  getPublicKey,
+  getBlankEvent,
+}
