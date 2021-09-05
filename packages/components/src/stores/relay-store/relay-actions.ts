435bf14db04a23d0986cba599edf18730b020574
Christopher David <chris@arcade.city>
2021-09-05 10:18:19 -0500

    sub to user from NostrDemo - same lib typeerror

--- packages/components/src/stores/relay-store/relay-actions.ts
+++ packages/components/src/stores/relay-store/relay-actions.ts
@@ -1,4 +1,4 @@
-import { display } from 'lib'
+import { display, log } from 'lib'
 import { RelayStore } from './relay-store'
 
 export const subscribeToUser = async (self: RelayStore, pubkey: string) => {
@@ -6,5 +6,22 @@ export const subscribeToUser = async (self: RelayStore, pubkey: string) => {
     name: 'subscribeToUser',
     preview: `Subscribing to ${pubkey}`,
   })
+
+  function onEvent(event: any, relay: any) {
+    relay && relay.url
+      ? console.log(
+          `got an event from ${relay.url} which is already validated.`,
+          event
+        )
+      : log(event)
+  }
+
+  self.env.relay.pool.sub({
+    cb: onEvent,
+    filter: {
+      author: pubkey,
+    },
+  })
+
   return true
 }
