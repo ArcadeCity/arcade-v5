12e8473a44e2593f3f3298fc476e56eb524111f4
Christopher David <chris@arcade.city>
2021-09-04 20:51:23 -0500

    Remove island-mobile folder

--- packages/island-mobile/index.js
+++
@@ -1,6 +0,0 @@
-import { AppRegistry } from 'react-native'
-import { name as appName } from './app.json'
-
-const { IslandApp } = require('@arcadecity/components/src/components/IslandApp')
-
-AppRegistry.registerComponent(appName, () => IslandApp)
