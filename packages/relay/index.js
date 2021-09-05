463f96a251734a7a545917f1f9bbaeb527d938fa
Christopher David <chris@arcade.city>
2021-09-04 21:15:54 -0500

    Ugly test of websocket working

--- packages/relay/index.js
+++ packages/relay/index.js
@@ -11,19 +11,33 @@ wsServer.on('connection', (socket) => {
   socket.on('message', (message) => console.log(message))
 })
 
+app.get('/', (req, res) => {
+  res.send(`Hello World!
+    <script>
+      // Create WebSocket connection.
+      const socket = new WebSocket('ws://localhost:3000');
+
+      // Connection opened
+      socket.addEventListener('open', function (event) {
+          socket.send('Hello Server!');
+      });
+
+      // Listen for messages
+      socket.addEventListener('message', function (event) {
+          console.log('Message from server ', event.data);
+      });
+    </script>
+  `)
+})
+
+const server = app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 // `server` is a vanilla Node.js HTTP server, so use
 // the same ws upgrade process described here:
 // https://www.npmjs.com/package/ws#multiple-servers-sharing-a-single-https-server
-app.on('upgrade', (request, socket, head) => {
+server.on('upgrade', (request, socket, head) => {
   wsServer.handleUpgrade(request, socket, head, (socket) => {
     wsServer.emit('connection', socket, request)
   })
-})
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
 })
