210eb14f0a7504f0f2bb3e28960c4e6a0175d6c3
Christopher David <chris@arcade.city>
2021-09-05 08:52:47 -0500

    Connect to freedom-relay

--- packages/components/src/services/relay/relay.ts
+++ packages/components/src/services/relay/relay.ts
@@ -2,9 +2,9 @@ import { relayPool } from 'nostr-tools'
 
 export class Relay {
   constructor() {
-    console.log('Created Relay')
     const pool = relayPool()
-    console.log('pool:', pool)
+    pool.addRelay('wss://freedom-relay.herokuapp.com/ws')
+    console.log('Created Relay with pool:', pool)
   }
 
   async setup() {}
