42f3eb60045afa13ffac1799ca57386d5a793ec1
Christopher David <chris@arcade.city>
2021-09-05 08:48:46 -0500

    Init relayPool()

--- packages/components/src/services/relay/relay.ts
+++ packages/components/src/services/relay/relay.ts
@@ -1,6 +1,10 @@
+import { relayPool } from 'nostr-tools'
+
 export class Relay {
   constructor() {
     console.log('Created Relay')
+    const pool = relayPool()
+    console.log('pool:', pool)
   }
 
   async setup() {}
