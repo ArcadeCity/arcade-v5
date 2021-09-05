5dcc41858d0dce712170df0e7b8f7d5a59a2fa4b
Christopher David <chris@arcade.city>
2021-09-04 21:06:23 -0500

    Set up websocket server

--- packages/relay/index.js
+++ packages/relay/index.js
@@ -1,7 +1,24 @@
 import express from 'express'
+import { WebSocketServer } from 'ws'
 
 const app = express()
 const port = 3000
+
+// Set up a headless websocket server that prints any
+// events that come in.
+const wsServer = new WebSocketServer({ noServer: true })
+wsServer.on('connection', (socket) => {
+  socket.on('message', (message) => console.log(message))
+})
+
+// `server` is a vanilla Node.js HTTP server, so use
+// the same ws upgrade process described here:
+// https://www.npmjs.com/package/ws#multiple-servers-sharing-a-single-https-server
+app.on('upgrade', (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, (socket) => {
+    wsServer.emit('connection', socket, request)
+  })
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
