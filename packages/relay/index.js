07b5c11984e858511113872ed1d7a4014b3e8056
Christopher David <chris@arcade.city>
2021-09-04 21:53:32 -0500

    [relay] Add initial websocket server (#32)
    * Set up websocket server
    
    * Ugly test of websocket working
    
    * Remove that
    
    * cleanup

--- packages/relay/index.js
+++ packages/relay/index.js
@@ -1,12 +1,25 @@
 import express from 'express'
+import { WebSocketServer } from 'ws'
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// Set up a headless websocket server that prints any events that come in.
+const wsServer = new WebSocketServer({ noServer: true })
+wsServer.on('connection', (socket) => {
+  socket.on('message', (message) => console.log(message))
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get('/', (req, res) => {
+  res.send('Arcade Relay')
+})
+
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
+
+server.on('upgrade', (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, (socket) => {
+    wsServer.emit('connection', socket, request)
+  })
 })
