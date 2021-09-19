72cf2950517b75b903f32cfed459b6657a94dfdb
Christopher David <chris@arcade.city>
2021-09-19 17:11:12 -0400

    ride - add typescript

--- packages/ride/src/index.js
+++ packages/ride/src/index.js
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'normalize.css/normalize.css'
 import './index.css'
-import App from './App'
+// import App from './App'
+import { App } from './NewApp'
 
 document.addEventListener('DOMContentLoaded', () => {
+  // ReactDOM.render(<></>, document.getElementById('root'))
   ReactDOM.render(<App />, document.getElementById('root'))
 })
