6523f682f170622605ef4c0eb8a68b4b459a7e62
Christopher David <chris@arcade.city>
2021-09-19 16:27:45 -0400

    [ride] Setup app as clone of explorer (#40)
    * Copy explorer folder to pkgs/ride
    
    * yarn install
    
    * add .env to project gitignore
    
    * Add color crap as devdep
    
    * add ride to readme
    
    * yarn versions - web builds too
    
    * Move explorer back into pkgs

---
+++ packages/ride/harp-gl-decoder.js
@@ -0,0 +1,4 @@
+import { OmvTileDecoderService, OmvTilerService } from '@here/harp-omv-datasource/index-worker';
+
+OmvTileDecoderService.start();
+OmvTilerService.start();
---
+++ packages/ride/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './index.css';
+import App from './App';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+});
