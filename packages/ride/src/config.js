27ccd33ea93943bb36c2be8c0f3cad2fce90059f
Christopher David <chris@arcade.city>
2021-09-19 14:56:44 -0400

    Copy explorer folder to pkgs/ride

---
+++ packages/ride/src/config.js
@@ -0,0 +1,19 @@
+export const apiKey = 'J0IJdYzKDYS3nHVDDEWETIqK3nAcxqW42vz7xeSq61M'
+
+// All open-source options:
+// berlin_tilezen_base.json
+// berlin_tilezen_day_reduced.json
+// berlin_tilezen_night_reduced.json
+// berlin_tilezen_effects_streets.json
+// berlin_tilezen_effects_outlines.json
+// export const theme = 'https://unpkg.com/@here/harp-map-theme@0.16.0/resources/berlin_tilezen_night_reduced.json';
+export const theme = '/resources/arcade.json'
+
+export const copyrightInfo = [
+  {
+    id: 'here.com',
+    year: new Date().getFullYear(),
+    label: 'HERE',
+    link: 'https://legal.here.com/terms',
+  },
+]
